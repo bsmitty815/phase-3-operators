e0f747cbfa80caae27d6f527abd477098cc39453
Brian Smith <brian.bubbam@gmail.com>
2021-07-12 13:29:29 -0500

    commit tree d55c42dd91536201226d79d6a902fc7828e6882d

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
 def unsafe?(speed)
-
+    if speed < 40
+        return true
+    elsif speed > 60
+        return true  
+    else # speed >= 40 && <= 60
+        return false      
+    end
+     #between 40 and 60 return false
 end
 
 
-
 def not_safe?(speed)
-	
+	return speed < 40 || speed > 60 ? true : false
 end
 	
 
